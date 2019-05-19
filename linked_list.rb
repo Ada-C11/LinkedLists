6402dc8c5c2acb6e2e88a2620229a9991934d464
Ariana Bray <aribray@gmail.com>
2019-05-18 19:20:41 -0700

    passing all linkedlist tests

--- linked_list.rb
+++ linked_list.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'node'
 
 class LinkedList
@@ -8,27 +10,56 @@ class LinkedList
   end
 
   def add_first(value)
-
+    new_node = Node.new(value)
+    new_node.next = @head unless @head.nil?
+    @head = new_node
   end
 
   def get_first
+    return nil if @head.nil?
 
+    @head.data
   end
 
   def length
-    return 0
+    current = @head
+    count = 0
+
+    until current.nil?
+      current = current.next
+      count += 1
+    end
+    count
   end
 
   def add_last(value)
-
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+    else
+      current = @head
+      current = current.next until current.next.nil?
+      current.next = new_node
+    end
   end
 
   def get_last
+    return nil if @head.nil?
 
+    current = @head
+    current = current.next until current.next.nil?
+    current.data
   end
 
   def get_at_index(index)
+    return nil if @head.nil?
 
+    count = 0
+    current = @head
+    while count != index && !current.next.nil?
+      current = current.next
+      count += 1
+    end
+    current.data
   end
-
 end
