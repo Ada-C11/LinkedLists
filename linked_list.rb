a2e94aea79b3807176ba2125ef54fc82807ee35f
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-05-22 21:42:04 -0700

    added linked list methods and passing all tests

--- linked_list.rb
+++ linked_list.rb
@@ -1,4 +1,5 @@
-require_relative 'node'
+require_relative "node"
+require "pry"
 
 class LinkedList
   attr_reader :head
@@ -8,27 +9,67 @@ class LinkedList
   end
 
   def add_first(value)
+    new_node = Node.new(value)
 
+    new_node.next = @head if !@head.nil?
+
+    @head = new_node
   end
 
   def get_first
-
+    return @head.nil? ? nil : @head.data
   end
 
   def length
-    return 0
+    current = @head
+    count = 0
+
+    while current != nil
+      current = current.next
+      count += 1
+    end
+
+    return count
   end
 
   def add_last(value)
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+      return
+    else
+      current = @head
+      while current.next != nil
+        current = current.next
+      end
 
+      return current.next = new_node
+    end
   end
 
   def get_last
+    return nil if @head.nil?
 
+    if @head.next.nil?
+      return @head.data
+    else
+      current = @head
+      while current.next != nil
+        current = current.next
+      end
+    end
+
+    return current.data
   end
 
   def get_at_index(index)
+    return if @head.nil?
+    current = @head
 
+    index.times do
+      current = current.next
+    end
+
+    return current.data
   end
-
 end
