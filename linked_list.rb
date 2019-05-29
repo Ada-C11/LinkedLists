d7362d66fc215c0fdd042bffe5b2af7e9b93369b
Shubha Rajan <shubharajan94@gmail.com>
2019-05-28 19:58:37 -0700

    all tests passing

--- linked_list.rb
+++ linked_list.rb
@@ -1,4 +1,5 @@
-require_relative 'node'
+require_relative "node"
+require "pry"
 
 class LinkedList
   attr_reader :head
@@ -8,27 +9,57 @@ class LinkedList
   end
 
   def add_first(value)
-
+    new_node = Node.new(value, @head)
+    @head = new_node
   end
 
   def get_first
-
+    return @head ? @head.data : nil
   end
 
   def length
-    return 0
+    length = 0
+    if !@head
+      return length
+    else
+      current = @head
+      while current
+        length += 1
+        current = current.next
+      end
+    end
+    return length
   end
 
   def add_last(value)
-
+    new_node = Node.new(value)
+    if !@head
+      @head = new_node
+    else
+      current = @head
+      while current.next
+        current = current.next
+      end
+      current.next = new_node
+    end
   end
 
   def get_last
-
+    return nil unless @head
+    current = @head
+    while current.next
+      current = current.next
+    end
+    return current.data
   end
 
   def get_at_index(index)
-
+    return nil unless @head
+    current = @head
+    index.times do
+      return nil unless current.next
+      current = current.next
+    end
+    return current.data
   end
-
 end
